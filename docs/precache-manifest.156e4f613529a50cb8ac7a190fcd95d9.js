486f5b051133a657789241cf0cd2e1ecb8796f70
Faisal Arkan <faisalarkan21@gmail.com>
2020-03-01 05:19:30 +0700

    add static file from build prod

---
+++ docs/precache-manifest.156e4f613529a50cb8ac7a190fcd95d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c649ddce4714910ce7b8458b3b5d02c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2e0f8dac166cc64873b",
+    "url": "/static/css/2.03d7f783.chunk.css"
+  },
+  {
+    "revision": "a87673c658d8131a080d",
+    "url": "/static/css/main.f2f5d8d1.chunk.css"
+  },
+  {
+    "revision": "d2e0f8dac166cc64873b",
+    "url": "/static/js/2.d78c6436.chunk.js"
+  },
+  {
+    "revision": "ef233db82b27231c46504582dee0c7bc",
+    "url": "/static/js/2.d78c6436.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a87673c658d8131a080d",
+    "url": "/static/js/main.223c1b0c.chunk.js"
+  },
+  {
+    "revision": "366078d72ab378f26168",
+    "url": "/static/js/runtime-main.e51ed5d0.js"
+  }
+]);
